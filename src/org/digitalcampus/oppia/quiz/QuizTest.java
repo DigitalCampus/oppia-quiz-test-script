2c21bee490c8df8b150679f2cb6cf2dc7c051857
alexlittle <alex@digital-campus.org>
2016-04-22 15:43:48 +0300

    initial commit

---
+++ src/org/digitalcampus/oppia/quiz/QuizTest.java
@@ -0,0 +1,13 @@
+package org.digitalcampus.oppia.quiz;
+
+public class QuizTest {
+
+	private String quizString = "";
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello");
+	}
+
+}
