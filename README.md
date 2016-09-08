a7b5a48a9688ac6f1154f7657d7c97f91c568ff3
alexlittle <alex@digital-campus.org>
2016-09-08 11:31:49 +0300

    add checker for openwash

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 oppia-quiz-test-script
 ===========================
+
+java -cp ./bin/ org.digitalcampus.oppia.quiz.QuizTest
+
--- src/org/digitalcampus/oppia/quiz/QuizTest.java
+++ src/org/digitalcampus/oppia/quiz/QuizTest.java
@@ -1,13 +1,19 @@
 package org.digitalcampus.oppia.quiz;
 
+import org.digitalcampus.mobile.quiz.Quiz;
+
 public class QuizTest {
 
 	private String quizString = "";
+	
+	
 	
 	public static void main(String[] args) {
 		
 		
 		System.out.println("hello");
+		
+		Quiz q = new Quiz();
 	}
 
 }
